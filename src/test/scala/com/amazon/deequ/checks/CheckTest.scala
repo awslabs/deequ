58ccca7d5d5ac377e82cec3ab2d646daf4160dbe
Stephan Seufert <seufert@amazon.com>
2019-01-25 17:09:46 +0100

    Fixes bug in right operand synthesis in "isContainedIn" check
    Right operand was wrongly constructed based on the "includeLowerBound" flag rather than the "includeUpperBound" flag.

--- src/test/scala/com/amazon/deequ/checks/CheckTest.scala
+++ src/test/scala/com/amazon/deequ/checks/CheckTest.scala
@@ -258,8 +258,8 @@ class CheckTest extends WordSpec with Matchers with SparkContextSpec with Fixtur
           .isContainedIn("att2", -1, 7, includeLowerBound = false, includeUpperBound = true)
 
       val numericRangeResults = runChecks(getDfWithNumericValues(sparkSession), numericRangeCheck1,
-        numericRangeCheck2, numericRangeCheck3, numericRangeCheck4, numericRangeCheck5,
-        numericRangeCheck6, numericRangeCheck7, numericRangeCheck8, numericRangeCheck9)
+        numericRangeCheck2, numericRangeCheck3, numericRangeCheck4, numericRangeCheck5, numericRangeCheck6,
+        numericRangeCheck7, numericRangeCheck8, numericRangeCheck9)
 
       assertEvaluatesTo(numericRangeCheck1, numericRangeResults, CheckStatus.Success)
       assertEvaluatesTo(numericRangeCheck2, numericRangeResults, CheckStatus.Error)
