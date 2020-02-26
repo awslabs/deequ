70a2a8bd0d35015e8aca6d60e05b6de71d9879ec
Robert Ambrus <v-rambrus@expediagroup.com>
2020-02-26 16:39:42 +0100

    Completeness checks for column combinations (#195)

--- src/test/scala/com/amazon/deequ/checks/CheckTest.scala
+++ src/test/scala/com/amazon/deequ/checks/CheckTest.scala
@@ -61,6 +61,29 @@ class CheckTest extends WordSpec with Matchers with SparkContextSpec with Fixtur
       assertEvaluatesTo(check3, context, CheckStatus.Warning)
     }
 
+    "return the correct check status for combined completeness" in
+      withSparkSession { sparkSession =>
+
+        val check1 = Check(CheckLevel.Error, "group-1")
+          .areComplete(Seq("item", "att1")) // 1.0
+          .haveCompleteness(Seq("item", "att1"), _ == 1.0) // 1.0
+
+        val check2 = Check(CheckLevel.Error, "group-2-E")
+          .haveCompleteness(Seq("item", "att1", "att2"), _ > 0.8) // 0.75
+
+        val check3 = Check(CheckLevel.Warning, "group-2-W")
+          .haveCompleteness(Seq("item", "att1", "att2"), _ > 0.8) // 0.75
+
+        val context = runChecks(getDfCompleteAndInCompleteColumns(sparkSession),
+          check1, check2, check3)
+
+        context.metricMap.foreach { println }
+
+        assertEvaluatesTo(check1, context, CheckStatus.Success)
+        assertEvaluatesTo(check2, context, CheckStatus.Error)
+        assertEvaluatesTo(check3, context, CheckStatus.Warning)
+    }
+
     "return the correct check status for uniqueness" in withSparkSession { sparkSession =>
 
       val check = Check(CheckLevel.Error, "group-1")
