4d3f195bb313f148640e67d5992759365e9a509d
York-Winegar, James M <james.m.york-winegar@accenture.com>
2020-04-05 18:52:56 -0500

    add a multi-column check to determine any data completeness over a set of columns

--- src/main/scala/com/amazon/deequ/checks/ColumnCondition.scala
+++ src/main/scala/com/amazon/deequ/checks/ColumnCondition.scala
@@ -1,22 +1,21 @@
 /**
- * Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"). You may not
- * use this file except in compliance with the License. A copy of the License
- * is located at
- *
- *     http://aws.amazon.com/apache2.0/
- *
- * or in the "license" file accompanying this file. This file is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- *
- */
-
+  * Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License"). You may not
+  * use this file except in compliance with the License. A copy of the License
+  * is located at
+  *
+  *     http://aws.amazon.com/apache2.0/
+  *
+  * or in the "license" file accompanying this file. This file is distributed on
+  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+  * express or implied. See the License for the specific language governing
+  * permissions and limitations under the License.
+  *
+  */
 package com.amazon.deequ.checks
 
-import org.apache.spark.sql.functions.{col}
+import org.apache.spark.sql.functions.col
 
 private[checks] object ColumnCondition {
 
@@ -24,6 +23,13 @@ private[checks] object ColumnCondition {
     cols
       .map(col(_).isNotNull)
       .reduce(_ and _)
+      .toString()
+  }
+
+  def isAnyNotNull(cols: Seq[String]): String = {
+    cols
+      .map(col(_).isNotNull)
+      .reduce(_ or _)
       .toString()
   }
 }
