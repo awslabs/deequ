a989ba3d082bb151ac5fe75e0c83787637888ad4
David Radford <david.samuel.radford@gmail.com>
2020-09-08 14:03:18 -0500

    Added spark profiles to build 2.2/3/4 and eventually 3.0 Added spark 3 compatibility and cleaned up deprecated code in tests Added test jar generation and spark shell testing helper

--- src/test/resources/EMRSparkShellTest.scala
+++ src/test/resources/EMRSparkShellTest.scala
@@ -19,13 +19,13 @@ For testing inside EMR or other flavors of spark cluster. Run commands after bui
 Add additional test classes as needed
 scala 2.12
 spark-shell -i <local_git_repo>/src/test/resources/EMRSparkShellTest.txt \
---packages org.scalatest:scalatest_2.12:3.1.2,org.scalamock:scalamock_2.12:4.4.0,org.scala-lang:scala-compiler:2.12.10, \
+--packages org.scalatest:scalatest_2.12:3.1.2,org.scalamock:scalamock_2.12:4.4.0,org.scala-lang:scala-compiler:2.12.10,\
 org.mockito:mockito-core:2.28.2,org.openjdk.jmh:jmh-core:1.23,org.openjdk.jmh:jmh-generator-annprocess:1.23,org.apache.datasketches:datasketches-java:1.3.0-incubating \
 --jars <local_git_repo>/target/deequ_2.12-1.1.0-SNAPSHOT.jar,<local_git_repo>/target/deequ_2.12-1.1.0-SNAPSHOT-tests.jar
 
 scala 2.11
 spark-shell -i <local_git_repo>/src/test/resources/EMRSparkShellTest.txt \
---packages org.scalatest:scalatest_2.11:3.1.2,org.scalamock:scalamock_2.11:4.4.0,org.scala-lang:scala-compiler:2.11.10, \
+--packages org.scalatest:scalatest_2.11:3.1.2,org.scalamock:scalamock_2.11:4.4.0,org.scala-lang:scala-compiler:2.11.10,\
 org.mockito:mockito-core:2.28.2,org.openjdk.jmh:jmh-core:1.23,org.openjdk.jmh:jmh-generator-annprocess:1.23,org.apache.datasketches:datasketches-java:1.3.0-incubating \
 --jars <local_git_repo>/target/deequ-1.1.0-SNAPSHOT.jar,<local_git_repo>/target/spark-deequ-testing/deequ-1.1.0-SNAPSHOT-tests.jar
  */
