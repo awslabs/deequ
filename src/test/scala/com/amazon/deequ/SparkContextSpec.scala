0ab4a2d46c69fc9da4e83a125295ae06a27d2809
Dario Bigongiari <dariobig@amazon.com>
2022-10-18 11:58:48 -0400

    fix scalastyle

--- src/test/scala/com/amazon/deequ/SparkContextSpec.scala
+++ src/test/scala/com/amazon/deequ/SparkContextSpec.scala
@@ -1,31 +1,29 @@
-/**
- * Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"). You may not
- * use this file except in compliance with the License. A copy of the License
- * is located at
- *
- *     http://aws.amazon.com/apache2.0/
- *
- * or in the "license" file accompanying this file. This file is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- *
- */
+/** Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License"). You may not
+  * use this file except in compliance with the License. A copy of the License
+  * is located at
+  *
+  * http://aws.amazon.com/apache2.0/
+  *
+  * or in the "license" file accompanying this file. This file is distributed on
+  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+  * express or implied. See the License for the specific language governing
+  * permissions and limitations under the License.
+  */
 
 package com.amazon.deequ
 
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.{SQLContext, SparkSession}
 
-/**
-  * To be mixed with Tests so they can use a default spark context suitable for testing
+/** To be mixed with Tests so they can use a default spark context suitable for
+  * testing
   */
 trait SparkContextSpec {
 
-  /**
-    * @param testFun thunk to run with SparkSession as an argument
+  /** @param testFun
+    *   thunk to run with SparkSession as an argument
     */
   def withSparkSession(testFun: SparkSession => Any): Unit = {
     val session = setupSparkSession
@@ -37,12 +35,13 @@ trait SparkContextSpec {
     }
   }
 
-  /**
-    * @param testFun thunk to run with SparkSession and SparkMonitor as an argument for the tests
-    *                that would like to get details on spark jobs submitted
-    *
+  /** @param testFun
+    *   thunk to run with SparkSession and SparkMonitor as an argument for the
+    *   tests that would like to get details on spark jobs submitted
     */
-  def withMonitorableSparkSession(testFun: (SparkSession, SparkMonitor) => Any): Unit = {
+  def withMonitorableSparkSession(
+      testFun: (SparkSession, SparkMonitor) => Any
+  ): Unit = {
     val monitor = new SparkMonitor
     val session = setupSparkSession
     session.sparkContext.addSparkListener(monitor)
@@ -53,31 +52,32 @@ trait SparkContextSpec {
     }
   }
 
-  /**
-    * @param testFun thunk to run with SparkContext as an argument
+  /** @param testFun
+    *   thunk to run with SparkContext as an argument
     */
   def withSparkContext(testFun: SparkContext => Any) {
     withSparkSession(session => testFun(session.sparkContext))
   }
 
-  /**
-    * @param testFun thunk to run with SQLContext as an argument
+  /** @param testFun
+    *   thunk to run with SQLContext as an argument
     */
   def withSparkSqlContext(testFun: SQLContext => Any) {
     withSparkSession(session => testFun(session.sqlContext))
   }
 
-  /**
-    * Setups a local sparkSession
+  /** Setups a local sparkSession
     *
-    * @return sparkSession to be used
+    * @return
+    *   sparkSession to be used
     */
   private def setupSparkSession = {
-    val session = SparkSession.builder()
+    val session = SparkSession
+      .builder()
       .master("local")
       .appName("test")
-      .config("spark.driver.host","localhost") // Machine ip
-      .config("spark.driver.bindAddress","localhost") // Machine ip
+      .config("spark.driver.host", "localhost") // Machine ip
+      .config("spark.driver.bindAddress", "localhost") // Machine ip
       .config("spark.ui.enabled", "false")
       .config("spark.sql.shuffle.partitions", 2.toString)
       .getOrCreate()
@@ -85,10 +85,10 @@ trait SparkContextSpec {
     session
   }
 
-  /**
-    * Tears down the sparkSession
+  /** Tears down the sparkSession
     *
-    * @param session Session to be stopped
+    * @param session
+    *   Session to be stopped
     * @return
     */
   private def tearDownSparkSession(session: SparkSession) = {
