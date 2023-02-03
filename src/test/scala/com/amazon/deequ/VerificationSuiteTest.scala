d29d837a662e4a4121457716c2a30b806463f366
Yannis Mentekidis <mentekid@amazon.com>
2023-02-03 13:43:15 -0500

    Refactor VerificationResult to correctly match Metrics to Analyzers

--- src/test/scala/com/amazon/deequ/VerificationSuiteTest.scala
+++ src/test/scala/com/amazon/deequ/VerificationSuiteTest.scala
@@ -85,24 +85,25 @@ class VerificationSuiteTest extends WordSpec with Matchers with SparkContextSpec
         }
       }
 
-    "generate a result that contains row-level results" in withSparkSession { session =>
+    "generate a result that 1" in withSparkSession { session =>
       val data = getDfCompleteAndInCompleteColumns(session)
 
-      val isComplete = new Check(CheckLevel.Error, "rule1").isComplete("att1")
-      val completeness = new Check(CheckLevel.Error, "rule2").hasCompleteness("att2", _ > 0.7)
-      val isPrimaryKey = new Check(CheckLevel.Error, "rule3").isPrimaryKey("item")
+      val isComplete = new Check(CheckLevel.Error, "rule1")
+        .isComplete("att1")
+        .isComplete("att2")
       val expectedColumn1 = isComplete.constraints.head.asInstanceOf[RowLevelConstraint].getColumnName
-      val expectedColumn2 = completeness.constraints.head.asInstanceOf[RowLevelConstraint].getColumnName
+      val expectedColumn2 = isComplete.constraints.tail.head.asInstanceOf[RowLevelConstraint].getColumnName
 
-      val suite = new VerificationSuite().onData(data).addChecks(Seq(isComplete, completeness, isPrimaryKey))
+      val suite = new VerificationSuite().onData(data).addChecks(Seq(isComplete))
 
       val result: VerificationResult = suite.run()
 
       assert(result.status == CheckStatus.Error)
 
       val resultData = VerificationResult.toRowLevelResults(session, result, data)
+      resultData.show()
 
-      val expectedColumns: Seq[String] = data.columns :+ expectedColumn1 :+ expectedColumn2
+      val expectedColumns: Seq[String] = data.columns :+ expectedColumn1
       assert(resultData.columns.sameElements(expectedColumns))
 
       val rowLevel1 = resultData.select(expectedColumn1).collect().map(r => r.getBoolean(0))
@@ -110,6 +111,44 @@ class VerificationSuiteTest extends WordSpec with Matchers with SparkContextSpec
 
       val rowLevel2 = resultData.select(expectedColumn2).collect().map(r => r.getBoolean(0))
       assert(Seq(true, true, false, true, false, true).sameElements(rowLevel2))
+
+    }
+
+    "generate a result that contains row-level results" in withSparkSession { session =>
+      val data = getDfCompleteAndInCompleteColumns(session)
+
+      val isComplete = new Check(CheckLevel.Error, "rule1").isComplete("att1")
+      val completeness = new Check(CheckLevel.Error, "rule2").hasCompleteness("att2", _ > 0.7)
+      val isPrimaryKey = new Check(CheckLevel.Error, "rule3").isPrimaryKey("item")
+      val maxLength = new Check(CheckLevel.Error, "rule4").hasMaxLength("att2", _ == 1)
+      val expectedColumn1 = isComplete.constraints.head.asInstanceOf[RowLevelConstraint].getColumnName
+      val expectedColumn2 = completeness.constraints.head.asInstanceOf[RowLevelConstraint].getColumnName
+      val expectedColumn3 = maxLength.constraints.head.asInstanceOf[RowLevelConstraint].getColumnName
+
+      val suite = new VerificationSuite().onData(data)
+        .addCheck(isComplete)
+        .addCheck(completeness)
+        .addCheck(isPrimaryKey)
+        .addCheck(maxLength)
+
+      val result: VerificationResult = suite.run()
+
+      assert(result.status == CheckStatus.Error)
+
+      val resultData = VerificationResult.toRowLevelResults(session, result, data)
+
+      resultData.show()
+      val expectedColumns: Seq[String] = data.columns :+ expectedColumn1 :+ expectedColumn2 :+ expectedColumn3
+      assert(resultData.columns.sameElements(expectedColumns))
+
+      val rowLevel1 = resultData.select(expectedColumn1).collect().map(r => r.getBoolean(0))
+      assert(Seq(true, true, true, true, true, true).sameElements(rowLevel1))
+
+      val rowLevel2 = resultData.select(expectedColumn2).collect().map(r => r.getBoolean(0))
+      assert(Seq(true, true, false, true, false, true).sameElements(rowLevel2))
+
+      val rowLevel3 = resultData.select(expectedColumn3).collect().map(r => r.getDouble(0))
+      assert(Seq(1.0, 1.0, 0.0, 1.0, 0.0, 0.0).sameElements(rowLevel3))
     }
 
     "accept analysis config for mandatory analysis" in withSparkSession { sparkSession =>
