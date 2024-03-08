6dc6b703ca36b1cff89511bdff37d5a371161b0c
rdsharma26 <65777064+rdsharma26@users.noreply.github.com>
2024-03-08 13:05:35 -0500

    [Min/Max] Apply filtered row behavior at the row level evaluation (#543)
    * [Min/Max] Apply filtered row behavior at the row level evaluation
    
    - This changes from applying the behavior at the analyzer level. It allows us to prevent the usage of MinValue/MaxValue as placeholder values for filtered rows.
    
    * Improved the separation of null rows, based on their source
    
    - Whether the outcome for a row is null because of being filtered out or due to the target column being null, is now stored in the outcome column itself.
    - We could have reused the placeholder value to find out if a row was originally filtered out, but that would not work if the actual value in the row was the same originally.
    
    * Mark filtered rows as true
    
    We recently fixed the outcome of filtered rows and made them default to true instead of false, which was a bug earlier. This change maintains that behavior.
    
    * Added null behavior - empty string to match block
    
    Not having it can cause match error.

--- src/main/scala/com/amazon/deequ/analyzers/Maximum.scala
+++ src/main/scala/com/amazon/deequ/analyzers/Maximum.scala
@@ -18,13 +18,11 @@ package com.amazon.deequ.analyzers
 
 import com.amazon.deequ.analyzers.Preconditions.{hasColumn, isNumeric}
 import org.apache.spark.sql.{Column, Row}
-import org.apache.spark.sql.functions.{col, max}
+import org.apache.spark.sql.functions.{col, element_at, max}
 import org.apache.spark.sql.types.{DoubleType, StructType}
 import Analyzers._
 import com.amazon.deequ.metrics.FullColumn
 import com.google.common.annotations.VisibleForTesting
-import org.apache.spark.sql.functions.expr
-import org.apache.spark.sql.functions.not
 
 case class MaxState(maxValue: Double, override val fullColumn: Option[Column] = None)
   extends DoubleValuedState[MaxState] with FullColumn {
@@ -43,13 +41,12 @@ case class Maximum(column: String, where: Option[String] = None, analyzerOptions
   with FilterableAnalyzer {
 
   override def aggregationFunctions(): Seq[Column] = {
-    max(criterion) :: Nil
+    max(element_at(criterion, 2).cast(DoubleType)) :: Nil
   }
 
   override def fromAggregationResult(result: Row, offset: Int): Option[MaxState] = {
-
     ifNoNullsIn(result, offset) { _ =>
-      MaxState(result.getDouble(offset), Some(rowLevelResults))
+      MaxState(result.getDouble(offset), Some(criterion))
     }
   }
 
@@ -60,19 +57,5 @@ case class Maximum(column: String, where: Option[String] = None, analyzerOptions
   override def filterCondition: Option[String] = where
 
   @VisibleForTesting
-  private def criterion: Column = conditionalSelection(column, where).cast(DoubleType)
-
-  private[deequ] def rowLevelResults: Column = {
-    val filteredRowOutcome = getRowLevelFilterTreatment(analyzerOptions)
-    val whereNotCondition = where.map { expression => not(expr(expression)) }
-
-    filteredRowOutcome match {
-      case FilteredRowOutcome.TRUE =>
-        conditionSelectionGivenColumn(col(column), whereNotCondition, replaceWith = Double.MinValue).cast(DoubleType)
-      case _ =>
-        criterion
-    }
-  }
-
+  private def criterion: Column = conditionalSelectionWithAugmentedOutcome(col(column), where, Double.MinValue)
 }
-
