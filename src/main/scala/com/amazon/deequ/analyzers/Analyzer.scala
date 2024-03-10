e4020d1a6e1ab66c0990ab9574b1fcc3520289bf
rdsharma26 <65777064+rdsharma26@users.noreply.github.com>
2024-03-10 17:08:37 -0400

    [MinLength/MaxLength] Apply filtered row behavior at the row level evaluation (#547)
    * [MinLength/MaxLength] Apply filtered row behavior at the row level evaluation
    
    - For certain scenarios, the filtered row behavior for MinLength and MaxLength was not working correctly.
    - For example, when using both minLength and maxLength constraints in a single check, and with both using == <value> as an assertion. This was resulting in the row level outcome of the filtered rows to be false. This was because we were replacing values for filtered rows for Min to MaxValue and for Max to MinValue. But a number could not equal both at the same time.
    - Updated the logic of the row level assertion to MinLength/MaxLength to match what was done for Min/Max.

--- src/main/scala/com/amazon/deequ/analyzers/Analyzer.scala
+++ src/main/scala/com/amazon/deequ/analyzers/Analyzer.scala
@@ -519,13 +519,16 @@ private[deequ] object Analyzers {
   }
 
   def conditionalSelectionWithAugmentedOutcome(selection: Column,
-                                               condition: Option[String],
-                                               replaceWith: Double): Column = {
+                                               condition: Option[String]): Column = {
     val origSelection = array(lit(InScopeData.name).as("source"), selection.as("selection"))
-    val filteredSelection = array(lit(FilteredData.name).as("source"), lit(replaceWith).as("selection"))
+
+    // The 2nd value in the array is set to null, but it can be set to anything.
+    // The value is not used to evaluate the row level outcome for filtered rows (to true/null).
+    // That decision is made using the 1st value which is set to "FilteredData" here.
+    val filteredSelection = array(lit(FilteredData.name).as("source"), lit(null).as("selection"))
 
     condition
-      .map { cond => when(not(expr(cond)), filteredSelection).otherwise(origSelection) }
+      .map { cond => when(expr(cond), origSelection).otherwise(filteredSelection) }
       .getOrElse(origSelection)
   }
 
