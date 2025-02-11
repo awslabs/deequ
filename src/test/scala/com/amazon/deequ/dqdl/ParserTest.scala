b48c7a12fded2bfdf918c97501763bad09a663d2
Andrius Juodelis <juodea@amazon.com>
2025-02-11 16:46:11 -0500

    add DQDL parser dependency

---
+++ src/test/scala/com/amazon/deequ/dqdl/ParserTest.scala
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2025 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ * use this file except in compliance with the License. A copy of the License
+ * is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ */
+
+package com.amazon.deequ.dqdl
+
+import org.scalatest.{Matchers, WordSpec}
+import software.amazon.glue.dqdl.parser.DQDLParser
+
+import scala.jdk.CollectionConverters.collectionAsScalaIterableConverter
+
+
+class ParserTest extends WordSpec with Matchers {
+
+  val parser = new DQDLParser()
+
+  "DQDL Parser" should {
+    "parse DQDL rules" in {
+
+      val ruleset = "Rules = [ RowCount > 1, ColumnCount = 3]"
+      val dqRuleset = parser.parse(ruleset)
+      val rules = dqRuleset.getRules.asScala
+
+      // Test number of rules
+      rules.size shouldBe 2
+
+      // Test individual rules
+      val rowCountRule = rules.find(_.getRuleType == "RowCount")
+      rowCountRule.isDefined shouldBe true
+      rowCountRule.map(_.toString) shouldBe Some("RowCount > 1")
+
+      val columnCountRule = rules.find(_.getRuleType == "ColumnCount")
+      columnCountRule.isDefined shouldBe true
+      columnCountRule.map(_.toString) shouldBe Some("ColumnCount = 3")
+    }
+  }
+}
