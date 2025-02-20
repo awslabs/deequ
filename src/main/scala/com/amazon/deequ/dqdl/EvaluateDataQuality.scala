ed4c4209ed8edc32baf7501f5282a891c1211a6c
Andrius <146995814+happy-coral@users.noreply.github.com>
2025-02-20 11:48:54 -0500

    scaffolding for checking data quality agains DQDL rulesets (#604)
    * add test for IllegalArgumentException
    
    * add main classes: rule convertes, translator, executor
    
    ---------
    
    Co-authored-by: Andrius Juodelis <juodea@amazon.com>

---
+++ src/main/scala/com/amazon/deequ/dqdl/EvaluateDataQuality.scala
@@ -0,0 +1,62 @@
+/**
+ * Copyright 2025 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ * use this file except in compliance with the License. A copy of the License
+ * is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ */
+
+package com.amazon.deequ.dqdl
+
+import com.amazon.deequ.checks.Check
+import org.apache.spark.sql.DataFrame
+import software.amazon.glue.dqdl.model.DQRuleset
+
+import scala.collection.convert.ImplicitConversions.`list asScalaBuffer`
+
+
+/**
+ * Entry point for evaluating data quality.
+ *
+ * Given a Spark DataFrame and a DQDL ruleset (as a String), this object:
+ *  - Parses and translates the rules to Deequ Checks.
+ *  - Executes the checks on the DataFrame.
+ *  - Translates the outcome back to a Spark DataFrame.
+ */
+object EvaluateDataQuality {
+
+  /**
+   * Validates the given Spark DataFrame against a set of data quality rules defined in DQDL format.
+   *
+   * <p>This method applies the specified data quality ruleset to the input DataFrame and returns a new
+   * DataFrame summarizing the overall quality results, including any issues detected during the analysis.</p>
+   *
+   * @param df      the Spark DataFrame to analyze.
+   * @param ruleset the data quality ruleset in DQDL format to apply to the DataFrame.
+   * @return a Spark DataFrame containing the aggregated data quality results.
+   */
+  def process(df: DataFrame, ruleset: String): DataFrame = {
+    // 1. Parse the ruleset
+    val dqRuleset: DQRuleset = DefaultDQDLParser.parse(ruleset)
+
+    // 2. Translate each DQDL rule into a corresponding Deequ Check.
+    val checks: Seq[Check] = dqRuleset.getRules.flatMap(rule => DQDLRuleTranslator.translateRule(rule))
+
+    // 3. Execute the checks against the DataFrame.
+    val executor = new DeequCheckExecutor
+    val verificationResult = executor.executeChecks(df, checks)
+
+    // 4. Translate the Deequ results into a Spark DataFrame.
+    val outcomeTranslator = new DeequOutcomeTranslator(df.sparkSession)
+    outcomeTranslator.translate(verificationResult)
+  }
+
+}
