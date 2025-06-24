74643a100952598c0d98c5e2cf0b795db1f3f198
Andrius Juodelis <juodea@amazon.com>
2025-06-24 13:53:54 -0400

    Add implementation for DQDL rules: DistinctValuesCount, Entropy, Mean, StandardDeviation Sum, UniqueValueRatio

---
+++ src/main/scala/com/amazon/deequ/dqdl/translation/rules/StandardDeviationRule.scala
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2025 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ * use this file except in compliance with the License. A copy of the License
+ * is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ */
+
+package com.amazon.deequ.dqdl.translation.rules
+
+import com.amazon.deequ.checks.{Check, CheckLevel}
+import com.amazon.deequ.dqdl.model.DeequMetricMapping
+import com.amazon.deequ.dqdl.translation.DQDLRuleConverter
+import com.amazon.deequ.dqdl.util.DQDLUtility.addWhereClause
+import software.amazon.glue.dqdl.model.DQRule
+import software.amazon.glue.dqdl.model.condition.number.NumberBasedCondition
+
+import scala.collection.JavaConverters._
+
+case class StandardDeviationRule() extends DQDLRuleConverter {
+  override def convert(rule: DQRule): Either[String, (Check, Seq[DeequMetricMapping])] = {
+    val col = rule.getParameters.asScala("TargetColumn")
+    val check = Check(CheckLevel.Error, java.util.UUID.randomUUID.toString)
+      .hasStandardDeviation(col, assertionAsScala(rule, rule.getCondition.asInstanceOf[NumberBasedCondition]))
+    Right((
+      addWhereClause(rule, check),
+      Seq(DeequMetricMapping("Column", col, "StandardDeviation", "StandardDeviation", None, rule = rule))))
+  }
+}
--- src/test/scala/com/amazon/deequ/dqdl/EvaluateDataQualitySpec.scala
+++ src/test/scala/com/amazon/deequ/dqdl/EvaluateDataQualitySpec.scala
@@ -131,6 +131,103 @@ class EvaluateDataQualitySpec extends AnyWordSpec with Matchers with SparkContex
       (row.getAs[Map[String, Double]]("EvaluatedMetrics").values.toSeq.head * 100).toInt should be(99)
     }
 
+    "support DistinctValuesCount rule" in withSparkSession { sparkSession =>
+      // given
+      val df = getDfWithNumericValues(sparkSession)
+      val ruleset = "Rules=[DistinctValuesCount \"att2\" = 4]"
+
+      // when
+      val results = EvaluateDataQuality.process(df, ruleset)
+
+      // then
+      val row = results.collect()(0)
+      row.getAs[String]("Outcome") should be("Passed")
+      row.getAs[Map[String, Double]]("EvaluatedMetrics").keys should contain("Column.att2.DistinctValuesCount")
+      // check the metric value
+      (row.getAs[Map[String, Double]]("EvaluatedMetrics").values.toSeq.head).toInt should be(4)
+    }
+
+    "support Entropy rule" in withSparkSession { sparkSession =>
+      // given
+      val df = getDfWithNumericValues(sparkSession)
+      val ruleset = "Rules=[Entropy \"att2\" > 1]"
+
+      // when
+      val results = EvaluateDataQuality.process(df, ruleset)
+
+      // then
+      val row = results.collect()(0)
+      row.getAs[String]("Outcome") should be("Passed")
+      row.getAs[Map[String, Double]]("EvaluatedMetrics").keys should contain("Column.att2.Entropy")
+      // check the metric value
+      (row.getAs[Map[String, Double]]("EvaluatedMetrics").values.toSeq.head * 100).toInt should be(124)
+    }
+
+    "support Mean rule" in withSparkSession { sparkSession =>
+      // given
+      val df = getDfWithNumericValues(sparkSession)
+      val ruleset = "Rules=[Mean \"att2\" = 3]"
+
+      // when
+      val results = EvaluateDataQuality.process(df, ruleset)
+
+      // then
+      val row = results.collect()(0)
+      row.getAs[String]("Outcome") should be("Passed")
+      row.getAs[Map[String, Double]]("EvaluatedMetrics").keys should contain("Column.att2.Mean")
+      // check the metric value
+      (row.getAs[Map[String, Double]]("EvaluatedMetrics").values.toSeq.head).toInt should be(3)
+    }
+
+    "support StandardDeviation rule" in withSparkSession { sparkSession =>
+      // given
+      val df = getDfWithNumericValues(sparkSession)
+      val ruleset = "Rules=[StandardDeviation \"att2\" > 3]"
+
+      // when
+      val results = EvaluateDataQuality.process(df, ruleset)
+
+      // then
+      val row = results.collect()(0)
+      row.getAs[String]("Outcome") should be("Passed")
+      row.getAs[Map[String, Double]]("EvaluatedMetrics").keys should contain("Column.att2.StandardDeviation")
+      // check the metric value
+      (row.getAs[Map[String, Double]]("EvaluatedMetrics").values.toSeq.head * 100).toInt should be(305)
+    }
+
+    "support Sum rule" in withSparkSession { sparkSession =>
+      // given
+      val df = getDfWithNumericValues(sparkSession)
+      val ruleset = "Rules=[Sum \"att2\" = 18]"
+
+      // when
+      val results = EvaluateDataQuality.process(df, ruleset)
+
+      // then
+      val row = results.collect()(0)
+      row.getAs[String]("Outcome") should be("Passed")
+      row.getAs[Map[String, Double]]("EvaluatedMetrics").keys should contain("Column.att2.Sum")
+      // check the metric value
+      row.getAs[Map[String, Double]]("EvaluatedMetrics").values.toSeq.head.toInt should be(18)
+    }
+
+    "support UniqueValueRatio rule" in withSparkSession { sparkSession =>
+      // given
+      val df = getDfWithNumericValues(sparkSession)
+      val ruleset = "Rules=[UniqueValueRatio \"att2\" > 0.7]"
+
+      // when
+      val results = EvaluateDataQuality.process(df, ruleset)
+
+      // then
+      val row = results.collect()(0)
+      print(row)
+      row.getAs[String]("Outcome") should be("Passed")
+      row.getAs[Map[String, Double]]("EvaluatedMetrics").keys should contain("Column.att2.UniqueValueRatio")
+      // check the metric value
+      (row.getAs[Map[String, Double]]("EvaluatedMetrics").values.toSeq.head * 100).toInt should be(75)
+    }
+
     "work with not yet supported rule" in withSparkSession { sparkSession =>
       // given
       val df = getDfFull(sparkSession)
